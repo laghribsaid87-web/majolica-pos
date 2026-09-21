2398eb07f0830c3ad6183680ced1a6976de4e84e
Majolica <majolica@example.com>
2026-09-21 23:12:50 +0100

    Fix firebase config for github actions and revert redirect to github

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // Pour configurer Firebase, vous devez créer un fichier .env.local à la racine du projet
 // et y coller vos identifiants Firebase.
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || ""
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDWAV15xxmD253EAVL_F-iye0O2zjSg4Ck",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "majolica-71ea6.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "majolica-71ea6",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "majolica-71ea6.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "683014494710",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:683014494710:web:632cea1531bde70d0f8fd7"
 };
 
 // Vérifie si la clé API existe
